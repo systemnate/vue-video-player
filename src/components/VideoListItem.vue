2c48a9aee351526c13645ffe2dfb9290432549c8
Nate Dalo <ndalo@merchantsbonding.com>
2018-10-29 08:58:21 -0500

    YouTube video player in Vue
    From VueJS essentials by Stephen Grider.

---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <li class="list-group-item media" @click="onVideOSelect">
+    <img class="mr3" :src="thumbnailUrl" />
+    <div class="media-body">
+      {{ video.snippet.title }}
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem',
+  props: ['video'],
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    }
+  },
+  methods: {
+    onVideOSelect() {
+      this.$emit('videoSelect', this.video);
+    }
+  }
+};
+</script>
+
+<style scoped>
+  li {
+    display: flex;
+    cursor: pointer;
+  }
+  li:hover {
+    background-color: #eee;
+  }
+  .media-body {
+    margin-left: 1%;
+  }
+</style>
